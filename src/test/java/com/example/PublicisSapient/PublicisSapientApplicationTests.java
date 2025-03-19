42844eb7423fb7aeb9d7517b9a29d0e7d6f224c0
kiranchavhan <chavhankiran123@gmail.com>
2025-03-19 23:05:05 +0530

    project setups

---
+++ src/test/java/com/example/PublicisSapient/PublicisSapientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PublicisSapient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicisSapientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
